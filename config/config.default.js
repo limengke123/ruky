a177b5fa221af31109634a221157ad43aa1b3233
limengke <752140235@qq.com>
2018-07-30 16:26:55 +0800

    egg项目学习

---
+++ config/config.default.js
@@ -0,0 +1,15 @@
+// keys 设置
+exports.keys = "myKeys"
+
+// 添加 view 配置
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+        '.tpl': 'nunjucks'
+    }
+}
+
+exports.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0'
+}
